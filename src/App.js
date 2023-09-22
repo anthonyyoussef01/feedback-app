a4605e8028fc72f4bcf88de51c6058f008d0ec01
anthony <elkommosyoussef.a@northeastern.edu>
2023-09-22 18:20:49 -0400

    Added the Feedback Stats which displays the number of reviews as well as the average rating

--- src/App.js
+++ src/App.js
@@ -2,20 +2,25 @@ import { useState } from "react";
 import Header from "./Components/Header";
 import FeedbackList from "./Components/FeedbackList";
 import FeedbackData from "./Data/FeedbackData";
+import FeedbackStats from "./Components/FeedbackStats";
 
 function App() {
     const [feedbacks, setFeedbacks] = useState(FeedbackData);
 
     const deleteFeedback = (id) => {
-        setFeedbacks(
-            feedbacks.filter((feedback) => feedback.id !== id )
-        )
+        // prompt user to confirm deletion of feedback
+        if (window.confirm("Are you sure you want to delete this feedback?")) {
+            setFeedbacks(
+                feedbacks.filter((feedback) => feedback.id !== id)
+            )
+        }
     };
 
     return (
         <>
             <Header />
             <div className={"container"}>
+                <FeedbackStats feedback={feedbacks} />
                 <FeedbackList feedback={feedbacks} handleDelete={deleteFeedback} />
             </div>
         </>
---
+++ src/Components/FeedbackStats.jsx
@@ -0,0 +1,23 @@
+import PropTypes from "prop-types";
+
+function FeedbackStats({feedback}) {
+	// Calculate the average rating
+	let avg = feedback.reduce((accumulator, currentValue) => {      // reduce in JS is like foldl in Racket
+		return accumulator + currentValue.rating
+	}, 0) / feedback.length;
+	// round to 1 decimal place and take off trailing 0s
+	avg = avg.toFixed(1).replace(/\.?0+$/, "");
+
+	return (
+		<div className="feedback-stats">
+			<h4>{feedback.length} Reviews</h4>
+			<h4>Average Rating: {isNaN(avg) ? 0 : avg}</h4>
+		</div>
+	);
+}
+
+FeedbackStats.propTypes = {
+	feedback: PropTypes.array.isRequired
+}
+
+export default FeedbackStats;
