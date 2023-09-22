50ebf39cd5e129de579dc23a9dd80f575ccf8897
anthony <elkommosyoussef.a@northeastern.edu>
2023-09-22 15:20:37 -0400

    FeedbackApp- Created App.js with a FeedbackList from FeedbackData Created FeedbackList created a list of FeedbackItem from the passed in feedback data Created FeedbackItem which makes a Card with a delete button Created Card which is just a stylized component, takes in data and stylizes it with the card (or reverse card) css styles

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import Header from "./Components/Header";
+import FeedbackList from "./Components/FeedbackList";
+import FeedbackData from "./Data/FeedbackData";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [feedbacks, setFeedbacks] = useState(FeedbackData);
+
+    const deleteFeedback = (id) => {
+        setFeedbacks(
+            feedbacks.filter((feedback) => feedback.id !== id )
+        )
+    };
+
+    return (
+        <>
+            <Header />
+            <div className={"container"}>
+                <FeedbackList feedback={feedbacks} handleDelete={deleteFeedback} />
+            </div>
+        </>
+    );
 }
 
 export default App;
---
+++ src/Components/Shared/Card.jsx
@@ -0,0 +1,25 @@
+import propTypes from 'prop-types';
+
+/*
+* This is just a styled component, meaning anything inside the <Card> tag will be styled with the card class
+*/
+
+function Card({children, reverse}) {
+	// return reverse ? (
+	// 		<div className="card reverse">{children}</div>
+	// ) : (
+	// 		<div className="card">{children}</div>
+	// );
+	return <div className={`card ${reverse && 'reverse'}`}>{children}</div>
+}
+
+Card.defaultProps = {
+	reverse: true,
+}
+
+Card.propTypes = {
+	children: propTypes.node.isRequired,
+	reverse: propTypes.bool,
+}
+
+export default Card;
