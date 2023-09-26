e9ca528f2085832501c28870a18a951c005b1d2f
anthony <elkommosyoussef.a@northeastern.edu>
2023-09-26 01:04:15 -0400

    added the loading text

--- src/Components/FeedbackList.jsx
+++ src/Components/FeedbackList.jsx
@@ -4,13 +4,15 @@ import FeedbackItem from "./FeedbackItem";
 import FeedbackContext from "./Context/FeedbackContext";
 
 function FeedbackList() {
-	const { feedback } = useContext(FeedbackContext);
+	const { feedback, isLoading } = useContext(FeedbackContext);
 
-	if (!feedback || feedback.length === 0) {
+	if (!isLoading && (!feedback || feedback.length === 0)) {
 		return <p>No feedback items</p>;
 	}
 
-	return (
+	return isLoading ? (
+		<h3>Loading...</h3>
+	) : (
 		<div className={"feedback-list"}>
 			<AnimatePresence>
 				{feedback.map((item) => (
