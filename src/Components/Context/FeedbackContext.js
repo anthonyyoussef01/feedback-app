ff4f82f8d9032e1ca7ea4862bd5b66bed0f29d11
anthony <elkommosyoussef.a@northeastern.edu>
2023-09-26 01:01:04 -0400

    now using a json server for the card data

--- src/Components/Context/FeedbackContext.js
+++ src/Components/Context/FeedbackContext.js
@@ -1,31 +1,28 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 import { v4 as uuidv4 } from "uuid";
 
 const FeedbackContext = createContext();
 
 export const FeedbackProvider = ({ children }) => {
-    const [feedback, setFeedback] = useState([
-        {
-            id: 1,
-            text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.',
-            rating: 8
-        },
-        {
-            id: 2,
-            text: 'This is Feedback 2',
-            rating: 9
-        },
-        {
-            id: 3,
-            text: 'The third feedback is here',
-            rating: 10
-        },
-    ]);
+    const [isLoading, setIsLoading] = useState(true);
+    const [feedback, setFeedback] = useState([]);
 
     const [feedbackEdit, setFeedbackEdit] = useState({
         item: {},
         edit: false,
     });
+
+    useEffect(() => {
+        fetchFeedback();
+    }, []);
+
+    const fetchFeedback = async () => {
+        const response = await fetch("http://localhost:5000/feedback?_sort=rating&_order=desc");
+        const data = await response.json();
+
+        setFeedback(data);
+        setIsLoading(false);
+    }
 
     const deleteFeedback = (id) => {
         // prompt user to confirm deletion of feedback
@@ -61,7 +58,13 @@ export const FeedbackProvider = ({ children }) => {
     return (
         <FeedbackContext.Provider
             value={{
-                feedback, feedbackEdit, deleteFeedback, addFeedback, editFeedback, updateFeedback
+                feedback,
+                feedbackEdit,
+                isLoading,
+                deleteFeedback,
+                addFeedback,
+                editFeedback,
+                updateFeedback
             }}
         >
             {children}
